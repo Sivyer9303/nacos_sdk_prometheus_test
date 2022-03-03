8af6449046c50e9427765596a4da70619b4f9bc2
sivyer9303 <sivyer9303@163.com>
2022-03-03 21:37:21 +0800

    使用v1.0尝试复现bug

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module prometheus_test
 go 1.16
 
 require (
-	github.com/nacos-group/nacos-sdk-go/v2 v2.0.0
-	github.com/prometheus/client_golang v1.11.0
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/nacos-group/nacos-sdk-go v1.1.0
 	github.com/prometheus/common v0.26.0
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
 )
 
-replace github.com/nacos-group/nacos-sdk-go/v2 => C:\Users\王冬\goPlace\nacos-sdk-go
+replace github.com/nacos-group/nacos-sdk-go => C:\Users\王冬\goPlace\nacos-sdk-go
--- serviceinfo_collector.go
+++ serviceinfo_collector.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 	"github.com/prometheus/common/log"
 	"net/http"
 	"strconv"
@@ -10,14 +13,40 @@ import (
 )
 
 func main() {
-	// 启动同名服务两个
+	// 启动同名服务
 	var wg sync.WaitGroup
-	tick := time.Tick(time.Second * 30)
+	tick := time.Tick(time.Second * 15)
 	portStart := 7788
-	for i := 0; i < 20; i++ {
+	// 启动一个线程，一直获取service list
+	go func() {
+		tick := time.Tick(time.Second * 5)
+		client, err := getNamingClient()
+		if err != nil {
+			log.Fatal("err ... ", err)
+		}
+		for {
+			select {
+			case <-tick:
+				_, err := client.GetService(vo.GetServiceParam{
+					ServiceName: "prometheuse_service",
+					GroupName:   "DEFAULT_GROUP",
+				})
+				if err != nil {
+					log.Fatal("get service fail ...", err)
+				}
+				//bytes, err := json.Marshal(service)
+				//if err != nil {
+				//	log.Fatal("Marshal service fail ...",err)
+				//}
+				//fmt.Println(string(bytes))
+			}
+		}
+	}()
+	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		select {
 		case <-tick:
+			// 每三十秒注册一个
 			go registerSelf(portStart+i, wg)
 		}
 	}
@@ -25,9 +54,13 @@ func main() {
 }
 
 func registerSelf(port int, wg sync.WaitGroup) {
+	log.Info("start register self")
 	defer wg.Done()
 	// 启动服务
-	namingClient, _ := getNamingClient()
+	namingClient, client_err := getNamingClient()
+	if client_err != nil {
+		log.Fatal("create naming client err ,", client_err)
+	}
 	// 先注册自己
 	_, err := namingClient.RegisterInstance(vo.RegisterInstanceParam{
 		Ip:          "localhost",
@@ -37,9 +70,37 @@ func registerSelf(port int, wg sync.WaitGroup) {
 		Ephemeral:   true,
 		Healthy:     true,
 		ServiceName: "prometheuse_service",
+		ClusterName: "ps",
 	})
 	if err != nil {
 		log.Fatal("register fail")
 	}
 	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+}
+
+func getNamingClient() (naming_client.INamingClient, error) {
+	var clientConfig = *constant.NewClientConfig(
+		//constant.WithNamespaceId("501689b2-129f-450c-8735-b04a5978b016"), //当namespace是public时，此处填空字符串。
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+		constant.WithUsername("nacos"),
+		constant.WithPassword("nacos"),
+	)
+	var serverConfigs = []constant.ServerConfig{
+		{
+			IpAddr:      "127.0.0.1",
+			ContextPath: "/nacos",
+			Port:        8848,
+			Scheme:      "http",
+		},
+	}
+	return clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfigs,
+		},
+	)
 }
